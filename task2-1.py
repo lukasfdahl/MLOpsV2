5c9e9fa3fd80c1bad39ed5f7b942c16408fdd45f
Lukasfdahl <112903921+lukasfdahl@users.noreply.github.com>
2026-06-06 19:28:04 +0200

    updated task 2-1 to produce acc and loss for each number

--- task2-1.py
+++ task2-1.py
@@ -3,6 +3,7 @@ from dataloader import get_mnist_loader
 from train import train_model
 from test import evaluate_model
 from device import DEVICE
+from torch.utils.data import DataLoader
 
 
 
@@ -11,11 +12,18 @@ model = model.to(DEVICE)
 train_loader = get_mnist_loader([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 val_loader_all = get_mnist_loader([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], train=False)
 
-val_loaders : list[]
+val_loaders : list[DataLoader] = []
+for i in range(0, 10):
+    val_loaders.append(get_mnist_loader([i], train=False))
 
 model = train_model(model, train_loader, val_loader_all, epochs=5, save_path="runs/task2-1")
 
 accuracy, average_loss = evaluate_model(model, val_loader_all)
 print("===================================")
 print(f"Final Accuracy: acc {accuracy} | loss {average_loss}")
+
+for i in range(0, 10):
+    accuracy, average_loss = evaluate_model(model, val_loaders[i])
+    print(f"{i} acc: {accuracy} | loss : {average_loss}")
+
 print("===================================")
